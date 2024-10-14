7365093fe4371aa6f42adb46bfd2742af8650fdd
LEVIZOO <nathannani06@gmail.com>
2024-10-14 21:58:02 +0100

    Add files via upload

---
+++ MANDELBROT.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float r,i;
+     float a = 0;
+    float b = 0;
+    int n = 0;
+
+    cout << "Entrez la partie reelle du nombre complexe : ";
+    cin >> r;
+    cout << "Entrez la partie imaginaire du nombre complexe : ";
+    cin >> i;
+
+
+    for (n=0;n < 30 ;n++) {
+        float a_new = a * a - b * b + r; 
+        b = 2 * a * b + i;              
+        a = a_new;
+
+}
+        if (a * a + b * b > 4) {
+            cout << "Le nombre complexe (" << r << ", " << i << ") diverge";
+}
+         else {
+           
+        cout << "Le nombre complexe (" << r << ", " << i << ") ne diverge pas ";
+     
+
+ }
+    
+
+    return 0;
+}
